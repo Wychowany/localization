b456b8b2c22e302c70aeecc0c4ff712f2960df30
jniewins <j.niewinski@stud.elka.pw.edu.pl>
2021-01-20 18:42:32 +0100

    dodanie dzielenia fft

--- get_phase_diff_with_frequencies.m
+++ get_phase_diff_with_frequencies.m
@@ -1,27 +1,27 @@
 function [PhDiff,base] = get_phase_diff_with_frequencies(x, y, treshold)
-x = x(:);
-y = y(:);
-fs = 8000;
-X = fft(x);
-Y = fft(y);
-N = size(Y,1);
-Ay = abs(Y);
-Ax = abs(X);
-Ay = Ay/N;
-Ax = Ax/N;
-Ay = Ay(1:floor(N/2+1));
-Ax = Ax(1:floor(N/2+1));
-Ay(2:end-1) = 2*Ay(2:end-1);
-Ax(2:end-1) = 2*Ax(2:end-1);
-f_step = fs/N;     % frequency step per one sample
-f = 0:f_step:fs/2; % frequency scale for the plot
-plot(Ax);
-j=1;
-for i = 1:size(f,2)
-    if Ax(i) > treshold
-        base(j) = fs/N * i; % frequency
-        PhDiff(j) = angle(Y(i)) - angle(X(i));
-        j = j + 1;
+    x = x(:);
+    y = y(:);
+    fs = 8000;    
+    X = fft(x);
+    Y = fft(y);
+    N = size(Y,1);
+    Ay = abs(Y);
+    Ax = abs(X);
+    Ay = Ay/N;
+    Ax = Ax/N;
+    Ay = Ay(1:floor(N/2+1));
+    Ax = Ax(1:floor(N/2+1));
+    Ay(2:end-1) = 2*Ay(2:end-1);
+    Ax(2:end-1) = 2*Ax(2:end-1);
+    f_step = fs/N;     % frequency step per one sample
+    f = 0:f_step:fs/2; % frequency scale for the plot
+%     plot(Ax);
+    j=1;
+    for i = 1:size(f,2)
+        if Ax(i) > treshold
+            base(j) = fs/N * i; % frequency
+            PhDiff(j) = angle(Y(i)) - angle(X(i));
+            j = j + 1;
+        end
     end
-end
 end
--- p.m
+++ p.m
@@ -1,20 +1,43 @@
-treshold = 0.0015;
+% treshold = 0.0015;
+treshold = 0;
 first = load('No_echo_#2_60deg.mat').audio_data;
-second = load('No_echo_#7_240deg.mat').audio_data;
+second = load('No_echo_#7_120deg.mat').audio_data;
 sizeFirst = size(first,1);
 sizeSecond = size(second,1);
+
 if sizeFirst > sizeSecond
     first = first(1:sizeSecond,:);
 elseif sizeSecond > sizeFirst
     second = second(1:sizeFirst,:); 
 end
+
 merged = (first + second)/2;
-x1 = first(:,1);
-x2 = first(:,2);
-x3 = first(:,3);
+% x1 = first(:,1);
+% x2 = first(:,2);
+% x3 = first(:,3);
 
-results12 = calculate_angles(x1,x2, treshold);
-histogram(results12,100);
+% x1 = second(:,1);
+% x2 = second(:,2);
+% x3 = second(:,3);
 
-% result23 = calculate_angles(x2,x3);
-% result31 = calculate_angles(x3,x1);
+x1 = merged(:,1);
+x2 = merged(:,2);
+x3 = merged(:,3);
+window_size = 256;
+no_windows = floor(size(x1,1)/window_size);
+suma = [];
+% sound(x1)
+figure;
+for i=1:no_windows
+    
+    results12 = calculate_angles(x3( (i-1)*window_size+1 : window_size*i ),x2((i-1)*window_size+1 : window_size*i), treshold);
+    suma = [suma results12];
+end
+histogram(suma,1000);
+
+% figure;
+% result23 = calculate_angles(x2,x3, treshold);
+% histogram(result23,100);
+% figure;
+% result31 = calculate_angles(x3,x1, treshold);
+% histogram(result31,100);
